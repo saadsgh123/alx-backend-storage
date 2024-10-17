d4fd77138fcd40e7dc26527aed1b478f589753e5
saadsgh123 <sdsghouri@gmail.com>
2024-10-17 16:01:19 +0100

    task 10 try 2 😅

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,18 +1,19 @@
--- Function to divide two number
-DELIMITER $$
-
-CREATE FUNCTION SafeDiv (
-    num1 INT,
-    num2 INT
+-- Script that creates a function SafeDiv that divides
+-- (and returns) the first by the second number or returns
+-- 0 if the second number is equal to 0.
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
 )
-RETURNS INT
+RETURNS FLOAT
 DETERMINISTIC
 BEGIN
-    IF num2 = 0 THEN
-       RETURN 0;
-    ELSE
-        RETURN num1 / num2;
-    END IF;
-END $$
-
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
 DELIMITER ;
